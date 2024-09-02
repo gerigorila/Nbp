b7c24515fd1a450cfb1e28bfcdcaa58eb2fc36a6
yaroslav.bondar <jb>
2024-09-02 18:23:54 +0200

    Add libraries: androidx.annotation, okhttp, retrofit.converter, adapters.

--- NbpApi/build.gradle.kts
+++ NbpApi/build.gradle.kts
@@ -13,4 +13,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.annotation)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation(libs.retrofit.adapters.result)
+    api(libs.okhttp)
 }
