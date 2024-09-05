f2dae314ce0c8b77d092c5120e530f67e4e5ad39
yaroslav.bondar <jb>
2024-09-05 18:19:50 +0200

    Added UI components to display the selected currency's rate information.

---
+++ features/nbp-main/ui/src/main/java/dev/bondar/ui/screens/currency_rate_info/CurrencyRateInfoScreen.kt
@@ -0,0 +1,89 @@
+package dev.bondar.ui.screens.currency_rate_info
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import dev.bondar.nbp_main.currency_rate_info.CurrencyState
+import dev.bondar.nbp_main.currency_rate_info.NbpCurrencyRateInfoViewModel
+import dev.bondar.uikit.NbpTheme
+
+
+@Composable
+fun CurrencyRateInfoScreen(modifier: Modifier = Modifier) {
+    val viewModel: NbpCurrencyRateInfoViewModel = hiltViewModel()
+    val state by viewModel.state.collectAsState()
+    val currentState = state
+
+    CurrencyRateInfoContent(currentState, modifier)
+}
+
+@Composable
+private fun CurrencyRateInfoContent(
+    currentState: CurrencyState,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier) {
+        when (currentState) {
+            is CurrencyState.None -> Unit
+            is CurrencyState.Error -> ErrorMessage(currentState)
+            is CurrencyState.Loading -> ProgressIndicator(currentState)
+            is CurrencyState.Success -> CurrencyRateList(currentState)
+        }
+    }
+}
+
+@Composable
+private fun ErrorMessage(
+    state: CurrencyState.Error,
+    modifier: Modifier = Modifier,
+) {
+    Column {
+        Box(
+            modifier
+                .fillMaxWidth()
+                .background(NbpTheme.colorScheme.error)
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Error during update", color = NbpTheme.colorScheme.onError)
+        }
+
+        val rates = state.rates
+        if (rates != null) {
+            CurrencyRateList(rates = rates, modifier = modifier)
+        }
+    }
+}
+
+@Composable
+private fun ProgressIndicator(
+    state: CurrencyState.Loading,
+    modifier: Modifier = Modifier,
+) {
+    Column {
+        Box(
+            modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+
+        val rates = state.rates
+        if (rates != null) {
+            CurrencyRateList(rates = rates, modifier = modifier)
+        }
+    }
+}
---
+++ features/nbp-main/ui/src/main/java/dev/bondar/ui/screens/currency_rate_info/CurrencyRateListContent.kt
@@ -0,0 +1,111 @@
+package dev.bondar.ui.screens.currency_rate_info
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.bondar.nbp.data.model.CurrencyRate
+import dev.bondar.nbp_main.currency_rate_info.CurrencyRateUI
+import dev.bondar.nbp_main.currency_rate_info.CurrencyState
+import dev.bondar.ui.screens.main.RateList
+import dev.bondar.uikit.NbpTheme
+
+
+@Composable
+internal fun CurrencyRateList(
+    rateState: CurrencyState.Success,
+    modifier: Modifier = Modifier,
+) {
+    CurrencyRateList(rates = rateState.rates, modifier)
+}
+
+@Composable
+internal fun CurrencyRateList(
+    rates: List<CurrencyRateUI>,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Row(
+            modifier = modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = rates.first().code.toString(),
+                style = NbpTheme.typography.headlineMedium
+            )
+            Text(
+                text = rates.first().currency.toString(),
+                style = NbpTheme.typography.headlineMedium
+            )
+        }
+
+        LazyColumn(modifier = modifier.fillMaxWidth()) {
+            items(rates) { ratesUI ->
+                key(ratesUI.code) {
+                    CurrencyRate(ratesUI)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun CurrencyRate(
+    rate: CurrencyRateUI,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(bottom = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            val date = rate.effectiveDate
+            if (date != null) {
+                Text(
+                    text = date,
+                    style = NbpTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    fontSize = 24.sp
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center,
+        ) {
+            val mid = rate.mid
+            if (mid != null) {
+                Text(
+                    text = mid.toString(),
+                    style = NbpTheme.typography.bodyMedium,
+                    maxLines = 3,
+                    fontSize = 24.sp,
+                )
+            }
+        }
+    }
+}
